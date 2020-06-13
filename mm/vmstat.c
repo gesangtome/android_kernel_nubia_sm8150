8d3b002da4299d20672d4246b58a2c4b2d9929ab
gesangtome <gesangtome@foxmail.com>
2020-06-13 17:31:02 +0800

    kernel: mm: Import uksm kernel patch
    You can find the patch source on https://github.com/dolohow/uksm
    
    Signed-off-by: gesangtome <gesangtome@foxmail.com>
    Change-Id: Ic858cc84749931d67043b31381b3d48fd05d4601

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1094,6 +1094,9 @@ const char * const vmstat_text[] = {
 	"nr_indirectly_reclaimable",
 	"nr_unreclaimable_pages",
 
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	/* enum writeback_stat_item counters */
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
